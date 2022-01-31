db3ee97a9eb55f8cc84a4633005584a26a689cb5
ronteparker <ronte.parker@icloud.com>
2022-01-30 20:48:21 -0500

    Fixed decimal point values and added thousands place separators.

--- PreWork/ViewController.swift
+++ PreWork/ViewController.swift
@@ -25,9 +25,9 @@ class ViewController: UIViewController {
         currency = defaults.string(forKey: "currency") ?? "$"
         
         //Update Tip Amount Label
-        tipAmountLabel.text = String(format: "\(currency)0.00")
+        tipAmountLabel.text = String(format: "\(currency)\(tipAmountLabel.text!.dropFirst())")
         //Update Total Amount
-        totalLabel.text = String(format: "\(currency)0.00")
+        totalLabel.text = String(format: "\(currency)\(totalLabel.text!.dropFirst())")
         
         if !defaults.bool(forKey: "customTipType") {
             tipControl.isEnabled = true
@@ -38,15 +38,17 @@ class ViewController: UIViewController {
             tipControl.isEnabled = false
             customTip.isEnabled = true
             customTipLabel.isEnabled = true
+            tipControl.selectedSegmentIndex = 0
         }
-        billAmountTextField.text = ""
-        customTip.text = ""
     }
      
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        billAmountTextField.becomeFirstResponder()
+        billAmountTextField.keyboardType = .decimalPad
+        customTip.keyboardType = .decimalPad
         
     }
     
@@ -60,26 +62,36 @@ class ViewController: UIViewController {
         
         //Get Total tip by multiplying tip
         let tipPercentages = [0.15, 0.18, 0.2]
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        var tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         
-        let total = bill + tip
+//        tip = Double(Int(tip * 100)) / 100.00
+        var total = bill + tip
+//        total = Double(Int(total * 100)) / 100.00
+        
+        let tipS: String = thousandsSeperator(tip)
+        let totalS: String = thousandsSeperator(total)
         
         //Update Tip Amount Label
-        tipAmountLabel.text = String(format: "\(currency)%.2f", tip)
+        tipAmountLabel.text = String(format: "\(currency)\(tipS)")
         //Update Total Amount
-        totalLabel.text = String(format: "\(currency)%.2f", total)
+        totalLabel.text = String(format: "\(currency)\(totalS)")
     }
     
     
     @IBAction func calculateCustomTip(_ sender: Any) {
         let bill = Double(billAmountTextField.text!) ?? 0
-        let tip = Double(customTip.text!) ?? 0
-        let total = bill + tip
+        var tip = Double(customTip.text!) ?? 0
+//        tip = Double(Int(tip * 100)) / 100.00
+        var total = bill + tip
+//        total = Double(Int(total * 100)) / 100.00
+        
+        let tipS: String = thousandsSeperator(tip)
+        let totalS: String = thousandsSeperator(total)
         
         //Update Tip Amount Label
-        tipAmountLabel.text = String(format: "\(currency)%.2f", tip)
+        tipAmountLabel.text = String(format: "\(currency)\(tipS)")
         //Update Total Amount
-        totalLabel.text = String(format: "\(currency)%.2f", total)
+        totalLabel.text = String(format: "\(currency)\(totalS)")
 
     }
     
@@ -88,6 +100,44 @@ class ViewController: UIViewController {
             calculateCustomTip(sender)
         } else {
             calculateTip(sender)
+        }
+    }
+    
+    
+    func thousandsSeperator(_ num: Double) -> String {
+       
+        if num > 999 {
+            let numString: String = String(Int(num))
+            
+            let numAfterDecimal = num - Double(Int(num))
+            let intAfterDecimal = Int(numAfterDecimal * 100)
+            
+            let ct = numString.count
+            let numOfCommas = ct / 3
+            var changedNum: String = String(numString.prefix(ct % 3))
+            
+            for _ in 1...numOfCommas {
+                changedNum.append("," + String(numString.prefix(3)))
+            }
+            if ct % 3 == 0 {
+                changedNum = String(changedNum.dropFirst())
+            }
+            
+            
+            changedNum.append(".\(intAfterDecimal)")
+            
+            return changedNum
+            
+        } else {
+            let numAfterDecimal = (num) - Double(Int(num))
+            let intAfterDecimal = Int(numAfterDecimal * 100)
+            if intAfterDecimal < 10 {
+                if intAfterDecimal == 0 {
+                    return String(Int(num)) + ".00"
+                }
+                return String(Int(num)) + ".0" + String(intAfterDecimal)
+            }
+            return String(Int(num)) + "." + String(intAfterDecimal)
         }
     }
     
